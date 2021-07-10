f6b6e8ad06501a589a0ddcc88a9a6b0a74258d6c
Henderson Andrade <32583573+hendersonandrade@users.noreply.github.com>
2021-07-09 22:41:34 -0300

    Add files via upload

---
+++ Calculate-AZStorageAccountFiles/Calculate-AZStorageAccountFiles.ps1
@@ -0,0 +1,24 @@
+$Context = New-AzStorageContext -StorageAccountName 'NAME OF STORAGE CONTEXT HERE' -StorageAccountKey 'STORAGE ACCOUNT KEY HERE'
+$container = Get-AzStorageContainer -name 'NAME OF STORAGE CONTAINER' -Context $Context
+
+$list = Import-CSV 'PATH OF CSV FILE'
+
+$Size = 0
+$ExceptionMessage = 0
+$ReadItems = 0
+
+foreach ($item in $list) {
+    $blob = $null
+    try {
+        $blob = Get-AzStorageBlob -container compline -Context $Context -Blob $item.FullPath -ErrorAction Stop
+        $size += $blob.Length
+        Write-Host "Size of files in bytes: $($Size)" -ForegroundColor Green
+        $ReadItems++
+        Write-Host "Number of files calculated: $($ReadItems)" -ForegroundColor Blue
+    }
+    catch {
+        Write-Error "File not found..."
+        $ExceptionMessage++
+        Write-Warning "Number of files not found: $($ExceptionMessage)"
+    }
+}
