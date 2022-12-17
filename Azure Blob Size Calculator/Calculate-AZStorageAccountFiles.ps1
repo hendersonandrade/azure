1c476dcf505c8027af53fc0c54282689f37e2ebc
Henderson Andrade <henderson.andrade@outlook.com>
2022-12-16 21:33:13 -0300

    Reorganize files in repo

---
+++ Azure Blob Size Calculator/Calculate-AZStorageAccountFiles.ps1
@@ -0,0 +1,23 @@
+$Context = New-AzStorageContext -StorageAccountName 'NAME OF STORAGE CONTEXT HERE' -StorageAccountKey 'STORAGE ACCOUNT KEY HERE'
+
+$list = Import-CSV 'PATH OF CSV FILE'
+
+$Size = 0
+$ExceptionMessage = 0
+$ReadItems = 0
+
+foreach ($item in $list) {
+    $blob = $null
+    try {
+        $blob = Get-AzStorageBlob -container container_name -Context $Context -Blob $item.FullPath -ErrorAction Stop
+        $size += $blob.Length
+        Write-Host "Size of files in bytes: $($Size)" -ForegroundColor Green
+        $ReadItems++
+        Write-Host "Number of files calculated: $($ReadItems)" -ForegroundColor Blue
+    }
+    catch {
+        Write-Error "File not found..."
+        $ExceptionMessage++
+        Write-Warning "Number of files not found: $($ExceptionMessage)"
+    }
+}
